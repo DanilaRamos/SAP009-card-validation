1508720aefa03466a140a9cc4683e72129c08fba
DanilaRamos <danila.ramos1985@gmail.com>
2023-01-21 19:58:30 -0300

    Cria CSS e HTML - Duvidas e Sobre

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
 
+function validation () {
+    
+    let cardNumber = document.getElementById("cardNumber").value;
+    console.log(validator.isValid(cardNumber));
+    let number = document.querySelector("cardNumber");
+
+    if (cardNumber === "") {
+        alert("Preencha todos os campos")
+    }
+
+    else if (isNaN(cardNumber)) {
+        alert("Digite somente números")
+    }
+
+    else if (!validator.isValid(cardNumber)) {
+        alert("Cartão inválido!")
+    }
+
+    else {
+        alert("Cartão " + validator.maskify(cardNumber) + " válido!")
+    }
+}
+
 console.log(validator);
